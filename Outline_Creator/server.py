0a2c4fcf1eca0481637b26f133cbb99835d5fa7c
Kunal Mundada <mundadakunal25@gmail.com>
2020-09-20 12:59:22 +0530

    XML file created in python

--- Outline_Creator/server.py
+++ Outline_Creator/server.py
@@ -1,7 +1,8 @@
-import os
 from flask import Flask, request, render_template
 import requests
+import os
 
+counter = 0
 app = Flask(__name__)
 
 @app.route('/handle_form', methods=['POST'])
@@ -13,8 +14,11 @@ def handle_form():
 
     if r.ok:
         print(r.status_code)
-        return r.text
-        # return "File uploaded!"
+        xml_str = (r.text)
+        save_path_file = f'source{counter}.xml'
+        with open(save_path_file, "w", encoding="utf-8") as f: 
+            f.write(xml_str)  
+        return f"XML file created: <{save_path_file}>"
     else:
         return "Error uploading file!"
 
