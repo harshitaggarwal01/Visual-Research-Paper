f40fd23f0f99d5e1c2a3375ac90462374a2f1a69
Kunal Mundada <mundadakunal25@gmail.com>
2020-09-21 11:04:59 +0530

    Minor changes in main.py

--- Main/main.py
+++ Main/main.py
@@ -9,36 +9,6 @@ import pprint
 from pyvis.network import Network
 from operator import itemgetter 
 
-####FLASK
-
-counter = 0
-app = Flask(__name__)
-
-@app.route('/handle_form', methods=['POST'])
-def handle_form():
-    print("Posted file: {}".format(request.files['file']))
-    file = request.files['file']
-    files = {"input": file.read()}
-    r = requests.post("https://cloud.science-miner.com/grobid/api/processFulltextDocument", files=files)
-
-    if r.ok:
-        print(r.status_code)
-        xml_str = (r.text)
-        save_path_file = f'source{counter}.xml'
-        with open(save_path_file, "w", encoding="utf-8") as f: 
-            f.write(xml_str)  
-        return f"XML file created: <{save_path_file}>"
-    else:
-        return "Error uploading file!"
-
-@app.route("/")
-def index():
-    return render_template("index.html");   
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
-
 ####SUMMARIZER
 def bert_summarize(document):
     model=Summarizer()
@@ -51,51 +21,53 @@ def bert_summarize(document):
 
 
 ####XML TO TEXT
+def xml2text(file_path):
+    tree = ET.parse(file_path)   # import xml from flask
+    root = tree.getroot()
 
-tree = ET.parse("source.xml")   # import xml from flask
-root = tree.getroot()
+    headings_seq = []
+    headings_name = []
+    headings_para = []
 
-headings_seq = []
-headings_name = []
-headings_para = []
+    headings_seq.append({'n' : '0 '})
+    headings_name.append("Abstract")
 
-headings_seq.append({'n' : '0 '})
-headings_name.append("Abstract")
+    for item in root.findall("./{http://www.tei-c.org/ns/1.0}text/{http://www.tei-c.org/ns/1.0}body/{http://www.tei-c.org/ns/1.0}div/{http://www.tei-c.org/ns/1.0}head"):
+        headings_seq.append(item.attrib)
+        headings_name.append(item.text)
 
-for item in root.findall("./{http://www.tei-c.org/ns/1.0}text/{http://www.tei-c.org/ns/1.0}body/{http://www.tei-c.org/ns/1.0}div/{http://www.tei-c.org/ns/1.0}head"):
-    headings_seq.append(item.attrib)
-    headings_name.append(item.text)
-
-mydoc = minidom.parse("source.xml")
-divs = mydoc.getElementsByTagName("div")
-for div in divs: #<div>
-    if(div.parentNode.nodeName == 'abstract'):
-        for elem in div.childNodes:
-            if(elem.nodeName == 'p'):
-                headings_para.append({"Abstract": elem.firstChild.data})
-
-    if(div.parentNode.nodeName == "body"):
-        try:
-            x = 'works'
-        except AttributeError:
-            continue
-        else:
+    mydoc = minidom.parse(file_path)
+    divs = mydoc.getElementsByTagName("div")
+    for div in divs: #<div>
+        if(div.parentNode.nodeName == 'abstract'):
             for elem in div.childNodes:
-                if(not elem.nodeName =='formula'):
-                    if(elem.nodeName == 'head'):
-                        section = elem.firstChild.data
-                        continue
-                    if(elem.nodeName == 'p'):
-                        headings_para.append({section : bert_summarize(elem.firstChild.data)})
+                if(elem.nodeName == 'p'):
+                    headings_para.append({"Abstract": elem.firstChild.data})
 
-# pp = pprint.PrettyPrinter(width=41, depth = 5, compact=True)
-# pp.pprint(headings_seq)
-# pp.pprint(headings_name)
-# pp.pprint(headings_para)
+        if(div.parentNode.nodeName == "body"):
+            try:
+                x = 'works'
+            except AttributeError:
+                continue
+            else:
+                for elem in div.childNodes:
+                    if(not elem.nodeName =='formula'):
+                        if(elem.nodeName == 'head'):
+                            section = elem.firstChild.data
+                            continue
+                        if(elem.nodeName == 'p'):
+                            headings_para.append({section : bert_summarize(elem.firstChild.data)})
+    # pp = pprint.PrettyPrinter(width=41, depth = 5, compact=True)
+    # pp.pprint(headings_seq)
+    # pp.pprint(headings_name)
+    # pp.pprint(headings_para)
+    create_MindMap(headings_name, headings_para)
+
+
 
 #### MINDMAP GENERATOR
 
-def create_MindMap(headings_names, headings_para): #list of headings and list of dicts
+def create_MindMap(headings_name, headings_para): #list of headings and list of dicts
     net = Network("The Visual Research Paper")
     root = "Your Visual Research Paper"
     #root_id = 1
@@ -124,3 +96,34 @@ def create_MindMap(headings_names, headings_para): #list of headings and list of
     net.toggle_physics(True)
     net.show_buttons(filter_=['physics'])
     net.show("mindmap.html")
+
+####FLASK
+
+counter = 0
+app = Flask(__name__)
+
+@app.route('/handle_form', methods=['POST'])
+def handle_form():
+    print("Posted file: {}".format(request.files['file']))
+    file = request.files['file']
+    files = {"input": file.read()}
+    r = requests.post("https://cloud.science-miner.com/grobid/api/processFulltextDocument", files=files)
+
+    if r.ok:
+        print(r.status_code)
+        xml_str = (r.text)
+        save_path_file = f'source{counter}.xml'
+        with open(save_path_file, "w", encoding="utf-8") as f: 
+            f.write(xml_str)
+        xml2text(save_path_file)
+    else:
+        return "Error uploading file!"
+
+@app.route("/")
+def index():
+    return render_template("index.html");   
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
